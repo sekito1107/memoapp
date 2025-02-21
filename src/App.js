5708c3cd5e7a906eda14edfc8a4b6335965cc831
sekito <sekito1107gg@gmail.com>
2025-02-22 04:12:29 +0900

    メモの選択機能を実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
   const [memoList, setMemoList] = useState([]);
   const [editingMemo, setEditingMemo] = useState(null);
-  let editingText = "";
+  const editingTextRef = useRef("");
 
   useEffect(() => {
     const savedMemo = JSON.parse(localStorage.getItem("memos")) || [];
@@ -23,16 +23,17 @@ export default function App() {
         [...prev, newMemo]
       )
       setEditingMemo(newMemo)
+      editingTextRef.current = newMemo.text; 
   }
 
   function handleTextChange(e) {
-    editingText = e.target.value;
+    editingTextRef.current = e.target.value;
   }
 
   function handleUpdateMemo() {
     setMemoList((prev) => 
       prev.map((memo) =>
-        memo.id === editingMemo.id ? {...memo, text: editingText} : memo
+        memo.id === editingMemo.id ? {...memo, text: editingTextRef.current} : memo
       )
     );
   
@@ -47,18 +48,31 @@ export default function App() {
     setEditingMemo(null);
   }
 
+  function startEditingMemo(memo) {
+    setEditingMemo(memo);
+    editingTextRef.current = memo.text;
+  }
+
   return (
     <>
       <h1>メモ一覧</h1>
       <ul>
         {memoList.map(memo =>
-          <li key={memo.id}>{memo.text.split("\n")[0]}</li>
+        <li key={memo.id} onClick={() => startEditingMemo(memo)}>
+          <span className={editingMemo?.id === memo.id ? "selected" : ""}>
+            {memo.text.split("\n")[0]}
+          </span>
+        </li>
         )}
       </ul>
       <button onClick={handleAddMemo}>+</button>
       {editingMemo &&
         <>
-          <textarea onChange={handleTextChange}>{editingMemo.text}</textarea>
+          <textarea
+            defaultValue={editingMemo.text} 
+            ref={(element) => element && (element.value = editingTextRef.current)}
+            onChange={handleTextChange} 
+          />
           <button onClick={handleUpdateMemo}>更新</button>
           <button onClick={handleDeleteMemo}>削除</button>
         </>
