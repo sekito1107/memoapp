71daf6d82e5b8bae29ba3d96940527ccf9a0cd68
sekito <sekito1107gg@gmail.com>
2025-02-23 00:10:21 +0900

    コンポーネントを分割

--- src/App.js
+++ src/App.js
@@ -1,89 +1,7 @@
-// import './App.css';
-// import { useState, useEffect, useRef } from 'react';
-
-// export default function App() {
-//   const [memoList, setMemoList] = useState([]);
-//   const [editingMemo, setEditingMemo] = useState(null);
-//   const editingTextRef = useRef("");
-
-//   useEffect(() => {
-//     const savedMemo = JSON.parse(localStorage.getItem("memos")) || [];
-//     setMemoList(savedMemo);
-//   },[])
-
-//   useEffect(() => {
-//     if (memoList.length > 0) {
-//       localStorage.setItem("memos", JSON.stringify(memoList));
-//     }
-//   }, [memoList]);
-
-//   function handleAddMemo() {
-//     const newMemo = {id: Date.now(), text: "新規メモ"}
-//       setMemoList(prev => 
-//         [...prev, newMemo]
-//       )
-//       setEditingMemo(newMemo)
-//       editingTextRef.current = newMemo.text; 
-//   }
-
-//   function handleTextChange(e) {
-//     editingTextRef.current = e.target.value;
-//   }
-
-//   function handleUpdateMemo() {
-//     setMemoList((prev) => 
-//       prev.map((memo) =>
-//         memo.id === editingMemo.id ? {...memo, text: editingTextRef.current} : memo
-//       )
-//     );
-  
-//     setEditingMemo(null);
-//   }
-
-//   function handleDeleteMemo() {
-//     setMemoList((prev) => 
-//       prev.filter((memo) => editingMemo.id !== memo.id)
-//     );
-
-//     setEditingMemo(null);
-//   }
-
-//   function startEditingMemo(memo) {
-//     setEditingMemo(memo);
-//     editingTextRef.current = memo.text;
-//   }
-
-//   return (
-//     <div className="app-container">
-//       <h1>メモ一覧</h1>
-//       <ul>
-//         {memoList.map(memo =>
-//         <li key={memo.id} onClick={() => startEditingMemo(memo)}>
-//           <span className={editingMemo?.id === memo.id ? "selected" : ""}>
-//             {memo.text.split("\n")[0]}
-//           </span>
-//         </li>
-//         )}
-//       </ul>
-//       <button onClick={handleAddMemo}>+</button>
-//       {editingMemo &&
-//         <div className="form-container">
-//           <textarea
-//             defaultValue={editingMemo.text} 
-//             ref={(element) => element && (element.value = editingTextRef.current)}
-//             onChange={handleTextChange} 
-//           />
-//           <button onClick={handleUpdateMemo}>更新</button>
-//           <button onClick={handleDeleteMemo}>削除</button>
-//         </div>
-//       }
-//     </div>
-//   );
-// }
-import './App.css';
-import { useState, useEffect, useRef } from 'react';
-import MemoList from './MemoList';
-import MemoForm from './MemoForm';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import MemoList from "./MemoList";
+import MemoForm from "./MemoForm";
 
 export default function App() {
   const [memoList, setMemoList] = useState([]);
@@ -93,7 +11,7 @@ export default function App() {
   useEffect(() => {
     const savedMemo = JSON.parse(localStorage.getItem("memos")) || [];
     setMemoList(savedMemo);
-  },[]);
+  }, []);
 
   useEffect(() => {
     if (memoList.length > 0) {
@@ -103,7 +21,7 @@ export default function App() {
 
   function handleAddMemo() {
     const newMemo = { id: Date.now(), text: "新規メモ" };
-    setMemoList(prev => [...prev, newMemo]);
+    setMemoList((prev) => [...prev, newMemo]);
     setEditingMemo(newMemo);
     editingTextRef.current = newMemo.text;
   }
@@ -113,16 +31,18 @@ export default function App() {
   }
 
   function handleUpdateMemo() {
-    setMemoList(prev =>
-      prev.map(memo =>
-        memo.id === editingMemo.id ? { ...memo, text: editingTextRef.current } : memo
-      )
+    setMemoList((prev) =>
+      prev.map((memo) =>
+        memo.id === editingMemo.id
+          ? { ...memo, text: editingTextRef.current }
+          : memo,
+      ),
     );
     setEditingMemo(null);
   }
 
   function handleDeleteMemo() {
-    setMemoList(prev => prev.filter(memo => editingMemo.id !== memo.id));
+    setMemoList((prev) => prev.filter((memo) => editingMemo.id !== memo.id));
     setEditingMemo(null);
   }
 
@@ -133,7 +53,11 @@ export default function App() {
 
   return (
     <div className="app-container">
-      <MemoList memoList={memoList} startEditingMemo={startEditingMemo} editingMemo={editingMemo} />
+      <MemoList
+        memoList={memoList}
+        startEditingMemo={startEditingMemo}
+        editingMemo={editingMemo}
+      />
       <button onClick={handleAddMemo}>+</button>
       {editingMemo && (
         <MemoForm
--- src/MemoForm.js
+++ src/MemoForm.js
@@ -1,11 +1,17 @@
-export default function MemoForm({ editingMemo, handleTextChange, handleUpdateMemo, handleDeleteMemo, editingTextRef }) {
+export default function MemoForm({
+  editingMemo,
+  handleTextChange,
+  handleUpdateMemo,
+  handleDeleteMemo,
+  editingTextRef,
+}) {
   return (
     <div>
       <textarea
-        defaultValue={editingMemo.text} 
-       ref={(element) => element && (element.value = editingTextRef.current)}
-        onChange={handleTextChange} 
-       />
+        defaultValue={editingMemo.text}
+        ref={(element) => element && (element.value = editingTextRef.current)}
+        onChange={handleTextChange}
+      />
       <button onClick={handleUpdateMemo}>更新</button>
       <button onClick={handleDeleteMemo}>削除</button>
     </div>
